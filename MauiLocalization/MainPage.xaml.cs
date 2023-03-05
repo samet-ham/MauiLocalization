bd041399be0ada5ceb515725a1d6a9f8f1d688a9
Samet Ham <59627331+samet-ham@users.noreply.github.com>
2023-03-05 15:02:54 +0300

    Add project files.

---
+++ MauiLocalization/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MauiLocalization.Resources.Languages;
+using System.Globalization;
+
+namespace MauiLocalization;
+
+public partial class MainPage : ContentPage
+{
+    public LocalizationResourceManager LocalizationResourceManager =>
+        LocalizationResourceManager.Instance;
+
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        var switchToCulture = AppResources.Culture.TwoLetterISOLanguageName.Equals("tr", StringComparison.InvariantCultureIgnoreCase) ? new CultureInfo("en-US") : new CultureInfo("tr-TR");
+
+        LocalizationResourceManager.Instance.SetCulture(switchToCulture);
+
+        count++;
+
+        CounterBtn.Text = String.Format(LocalizationResourceManager["Counter"].ToString(), count);
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
---
+++ MauiLocalization/TraslateExtansion.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiLocalization;
+
+[ContentProperty(nameof(Name))]
+public class TraslateExtansion : IMarkupExtension<BindingBase>
+{
+    public string Name { get; set; }
+    public BindingBase ProvideValue(IServiceProvider serviceProvider)
+    {
+        return new Binding
+        {
+            Mode = BindingMode.OneWay,
+            Path = $"[{Name}]",
+            Source = LocalizationResourceManager.Instance
+        };
+    }
+
+    object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
+    {
+        return ProvideValue(serviceProvider);
+    }
+}
